6da3c0dbc888d99d752d279b648c86e3bffc53b6
pbb <bourdeau.phil@gmail.com>
2020-03-04 22:49:25 -0500

    Basic application scaffolding

---
+++ src/business/calculator/DiscountedCashFlow.ts
@@ -0,0 +1,48 @@
+export class DiscountCashFlowCalculator {
+  /** number of years before perpetuity **/
+  private YEARS = 10;
+  /** discount rate **/
+  private DR: number;
+  /** GR **/
+  private GR: number;
+  /** long term growth rate **/
+  private LTGR = 0.03;
+  private baseFreeCashFlow: number;
+
+
+  constructor(DR: number, GR: number, baseFreeCashFlow: number) {
+    this.DR = DR;
+    this.GR = GR;
+    this.baseFreeCashFlow = baseFreeCashFlow;
+  }
+
+  /**
+   * Calculate the free cash flow in a number of years with determined GR
+   * @param years
+   */
+  cashFlowInYears(years: number): number {
+    return this.baseFreeCashFlow * this.rateOverYears(this.GR, years)
+  }
+
+  discountedCashFlowBeforePerpetuity(): number {
+    return this.cashFlowInYears(this.YEARS) / this.rateOverYears(this.DR, this.YEARS)
+  }
+
+  discountedCashFlowPerpetuity(): number {
+    return (this.cashFlowInYears(this.YEARS + 1) * (1 + this.LTGR) /
+      (this.DR - this.LTGR)) * (1 / this.rateOverYears(this.DR, this.YEARS))
+  }
+
+  intrinsicValue(): number {
+    return this.discountedCashFlowBeforePerpetuity() + this.discountedCashFlowPerpetuity()
+  }
+
+  /**
+   * Calculate a factor over a number of years
+   * @param years
+   * @param rate
+   */
+  rateOverYears(rate: number, years: number): number {
+    return (1 + rate) ** years
+  }
+}
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,30 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <label for="discount_rate">Discount Rate</label>
+    <input id="discount_rate" type="number" v-model="discount_rate">
+
+    <label for="growth_rate">Growth Rate</label>
+    <input id="growth_rate" type="number" v-model="growth_rate">
+
+    <div v-if="discount_rate > 0 && growth_rate > 0">
+      {{discountedCashFlow}}
+    </div>
   </div>
 </template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator'
+import { DiscountCashFlowCalculator } from '@/business/calculator/DiscountedCashFlow'
+
+  @Component
+export default class HelloWorld extends Vue {
+    private growth_rate = 0;
+    private discount_rate = 0;
+
+    get discountedCashFlow (): number {
+      const calculator = new DiscountCashFlowCalculator(this.discount_rate, this.growth_rate, 1000)
+
+      return calculator.intrinsicValue()
+    }
+}
+</script>
