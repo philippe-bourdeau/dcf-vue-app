0159a0a9cb09f41715514fece1d779cdf6a69651
pbb <bourdeau.phil@gmail.com>
2020-03-04 23:27:50 -0500

    switch to eslint for typescript files

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,15 +4,17 @@ module.exports = {
     node: true
   },
   extends: [
+    "plugin:@typescript-eslint/recommended",
     'plugin:vue/essential',
     '@vue/standard',
     '@vue/typescript/recommended'
   ],
   parserOptions: {
-    ecmaVersion: 2020
+    ecmaVersion: 2020,
+    parser: "@typescript-eslint/parser"
   },
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
   }
 }
