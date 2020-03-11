c6a3d1521922dcd4e464adaba0ae1d6648910fc4
pbb <bourdeau.phil@gmail.com>
2020-03-11 07:25:40 -0400

    Scaffolding of financial statements

--- src/business/shapes/FinancialStatement.ts
+++ src/business/shapes/FinancialStatement.ts
@@ -1,7 +1,11 @@
 import { BalanceSheet } from '@/business/shapes/BalanceSheet'
+import { CashFlowStatement } from '@/business/shapes/CashFlowStatement'
+import { IncomeStatement } from '@/business/shapes/IncomeStatement'
 
 export interface FinancialStatement {
   ticker: string;
   year: string;
   balanceSheet: BalanceSheet;
+  cashFlowStatement: CashFlowStatement;
+  incomeStatement: IncomeStatement;
 }
