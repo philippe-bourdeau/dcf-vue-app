667f3acb574654f3890e8e132a8e7eb761c80a89
pbb <bourdeau.phil@gmail.com>
2020-03-12 11:49:22 -0400

    tweaked some eslint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -16,5 +16,7 @@ module.exports = {
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'camelcase': 'off',
+    "@typescript-eslint/camelcase": ["error", { "properties": "never" }]
   }
 }
